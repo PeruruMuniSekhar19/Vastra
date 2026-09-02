030a5f5a29d947ad88be6b8dc8172eb5e0f7b3f8
PeruruMuniSekhar19 <munisekhar496@gmail.com>
2026-09-02 11:00:02 +0530

    final admin

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,19 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-
-try:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
-        print("ADMIN CREATED ")
-    else:
-        u = User.objects.get(username='admin')
-        u.set_password('admin123')
-        u.is_staff = True
-        u.is_superuser = True
-        u.save()
-        print("ADMIN RESET")
-except Exception as e:
-    print(f"Admin creation failed: {e}")
